b02a7de358fd3deefb430789fb0545160589d6fc
Jonathan Dieter <jdieter@gmail.com>
2018-05-04 18:08:45 +0300

    Make zck_read_header a proper utility with arguments and help.
    Signed-off-by: Jonathan Dieter <jdieter@gmail.com>

--- src/zck_read_header.c
+++ src/zck_read_header.c
@@ -31,17 +31,81 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <argp.h>
 #include <zck.h>
 
-int main (int argc, char *argv[]) {
-    if(argc != 2) {
-        printf("Usage: %s <file>\n", argv[0]);
-        exit(1);
-    }
+#include "util_common.h"
 
-    int src_fd = open(argv[1], O_RDONLY);
+static char doc[] = "zck_read_header - Read header from a zchunk file";
+
+static char args_doc[] = "<file>";
+
+static struct argp_option options[] = {
+    {"verbose", 'v', 0,        0,
+     "Increase verbosity (can be specified more than once for debugging)"},
+    {"quiet",   'q', 0,        0, "Only show errors"},
+    {"version", 'V', 0,        0, "Show program version"},
+    { 0 }
+};
+
+struct arguments {
+  char *args[1];
+  zck_log_type log_level;
+};
+
+static error_t parse_opt (int key, char *arg, struct argp_state *state) {
+    struct arguments *arguments = state->input;
+
+    switch (key) {
+        case 'v':
+            arguments->log_level -= 1;
+            if(arguments->log_level < ZCK_LOG_DEBUG)
+                arguments->log_level = ZCK_LOG_DEBUG;
+            break;
+        case 'q':
+            arguments->log_level = ZCK_LOG_ERROR;
+            break;
+        case 'V':
+            version();
+            break;
+
+        case ARGP_KEY_ARG:
+            if (state->arg_num >= 1) {
+                argp_usage (state);
+                return EINVAL;
+            }
+            arguments->args[state->arg_num] = arg;
+
+            break;
+
+        case ARGP_KEY_END:
+            if (state->arg_num < 1) {
+                argp_usage (state);
+                return EINVAL;
+            }
+            break;
+
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+static struct argp argp = {options, parse_opt, args_doc, doc};
+
+int main (int argc, char *argv[]) {
+    struct arguments arguments = {0};
+
+    /* Defaults */
+    arguments.log_level = ZCK_LOG_WARNING;
+
+    argp_parse (&argp, argc, argv, 0, 0, &arguments);
+
+    zck_set_log_level(arguments.log_level);
+
+    int src_fd = open(arguments.args[0], O_RDONLY);
     if(src_fd < 0) {
-        printf("Unable to open %s\n", argv[1]);
+        printf("Unable to open %s\n", arguments.args[0]);
         perror("");
         exit(1);
     }

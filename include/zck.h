6508d090250e04b17c61cd15ac63c99c3ff953bb
Jonathan Dieter <jdieter@gmail.com>
2018-06-08 14:57:08 +0300

    Bump to 0.7.0 with massive API changes
    Signed-off-by: Jonathan Dieter <jdieter@gmail.com>

--- include/zck.h
+++ include/zck.h
@@ -1,10 +1,10 @@
 #ifndef ZCK_H
 #define ZCK_H
 
-#define ZCK_VERSION "0.6.3"
+#define ZCK_VERSION "0.7.0"
 #define ZCK_VER_MAJOR 0
-#define ZCK_VER_MINOR 6
-#define ZCK_VER_REVISION 3
+#define ZCK_VER_MINOR 7
+#define ZCK_VER_REVISION 0
 #define ZCK_VER_SUBREVISION 0
 
 #define True 1

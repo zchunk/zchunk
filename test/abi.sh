12f34b3db9a9a0f2fee197630f5ec4a027397f41
Jonathan Dieter <jdieter@gmail.com>
2018-06-11 22:12:48 +0300

    Use public headers for abi test
    Signed-off-by: Jonathan Dieter <jdieter@gmail.com>

--- test/abi.sh
+++ test/abi.sh
@@ -15,14 +15,14 @@ find abi -type l -delete
 
 # Redump abi/stable
 cd abi/stable
-abi-dumper libzck.so.* -lver `cat zck.h | grep ZCK_VERSION | head -n 1 | awk '{ print $3 }' | sed s/\"//g`
+abi-dumper libzck.so.* -lver `cat zck.h | grep ZCK_VERSION | head -n 1 | awk '{ print $3 }' | sed s/\"//g` -public-headers `pwd abi/stable`
 if [ "$?" -ne 0 ]; then
     exit 1
 fi
 
 # Dump abi/new
 cd ../new
-abi-dumper libzck.so.* -lver `cat zck.h | grep ZCK_VERSION | head -n 1 | awk '{ print $3 }' | sed s/\"//g`
+abi-dumper libzck.so.* -lver `cat zck.h | grep ZCK_VERSION | head -n 1 | awk '{ print $3 }' | sed s/\"//g` -public-headers `pwd abi/new`
 if [ "$?" -ne 0 ]; then
     exit 1
 fi

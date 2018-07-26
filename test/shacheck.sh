6913e87e84ed3d77f88086d090b5a39d30e0bf27
Jonathan Dieter <jdieter@gmail.com>
2018-07-26 15:35:04 +0100

    Update shacheck script so it can ran command with variable arguments
    Signed-off-by: Jonathan Dieter <jdieter@gmail.com>

--- test/shacheck.sh
+++ test/shacheck.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
-"$1" "$2"
+CMD="$1"
+EOUTPUT="$2"
+ECHKSUM="$3"
+shift 3
+"$CMD" $@
+
 if [ "$?" -ne 0 ]; then
     exit 1
 fi
 
-ls -l
-
-CHKSUM=$(sha256sum "$3" | awk '{ print $1 }')
-if [ "$CHKSUM" != "$4" ]; then
+CHKSUM=$(sha256sum "$EOUTPUT" | awk '{ print $1 }')
+if [ "$CHKSUM" != "$ECHKSUM" ]; then
+    echo "Calculated checksum: $CHKSUM"
+    echo "Expected checksum: $ECHKSUM"
     echo "Checksum doesn't match"
     exit 1
 fi
+
+rm "$EOUTPUT" -f
+exit 0

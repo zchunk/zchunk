5715157da35898a7cb3293b7a5394ba23715b5a4
Jonathan Dieter <jdieter@gmail.com>
2020-12-08 20:34:35 +0000

    New release
    Signed-off-by: Jonathan Dieter <jdieter@gmail.com>

--- test/abi/stable/zck.h
+++ test/abi/stable/zck.h
@@ -1,7 +1,7 @@
 #ifndef ZCK_H
 #define ZCK_H
 
-#define ZCK_VERSION "1.1.7"
+#define ZCK_VERSION "1.1.8"
 
 #include <stdlib.h>
 #include <stdbool.h>
--- test/new_abi.sh
+++ test/new_abi.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 
+rm -rf ../build/src/lib/libzck.so.*.*.*.p 2>/dev/null
+
 if [ ! -e ../build/src/lib/libzck.so.*.*.* ]; then
     echo "No library exists"
     exit 1

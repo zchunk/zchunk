166287bf83e7d8d026fb7e10726d812d71833940
Jonathan Dieter <jdieter@gmail.com>
2021-05-01 18:16:06 +0100

    Fix type mismatch
    Signed-off-by: Jonathan Dieter <jdieter@gmail.com>

--- src/lib/comp/nocomp/nocomp.h
+++ src/lib/comp/nocomp/nocomp.h
@@ -1,6 +1,6 @@
 #ifndef ZCHUNK_COMPRESSION_NOCOMP_H
 #define ZCHUNK_COMPRESSION_NOCOMP_H
 
-int nocomp_setup(zckCtx *zck, zckComp *comp);
+bool nocomp_setup(zckCtx *zck, zckComp *comp);
 
 #endif
--- src/lib/comp/zstd/zstd.h
+++ src/lib/comp/zstd/zstd.h
@@ -1,6 +1,6 @@
 #ifndef ZCHUNK_COMPRESSION_ZSTD_H
 #define ZCHUNK_COMPRESSION_ZSTD_H
 
-int zstd_setup(zckCtx *zck, zckComp *comp);
+bool zstd_setup(zckCtx *zck, zckComp *comp);
 
 #endif

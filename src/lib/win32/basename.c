ce7ddd7b176c10478ce369701c9f300550c6b2c3
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-01-06 18:32:22 +0100

    support older meson, fix macos

--- src/lib/win32/basename.c
+++ src/lib/win32/basename.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include <windows.h>
 
+// note this is not a proper basename implementation
 char* basename(char* path)
 {
-    // note this is not a proper basename implementation
     char *p = strrchr (path, '\\');
     if (p == NULL)
     {
@@ -11,21 +10,4 @@ char* basename(char* path)
         p = strrchr(path, '/');
     }
     return p ? p + 1 : (char *) path;
-
-    // char full_path[MAX_PATH], drive[MAX_PATH], dir[MAX_PATH], filename[MAX_PATH], ext[MAX_PATH];
-
-    // printf("Input: %s", path);
-
-    // _fullpath(full_path, path, MAX_PATH);
-    // printf("Input: %s", full_path);
-
-    // _splitpath(full_path, drive, dir, filename, ext);
-
-    // printf("%s, %s, %s, %s", drive, dir, filename, ext);
-
-    // const char* res = malloc(MAX_PATH);
-    // sprintf(res, "%s%s", filename, ext);
-    // printf("Result: %s", res);
-
-    // return res;
 }
